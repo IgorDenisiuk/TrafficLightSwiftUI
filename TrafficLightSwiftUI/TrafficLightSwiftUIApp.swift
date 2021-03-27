939e19363ea4c4b0a18dec0e30c6859e343d926e
IgorDenisiuk <73896271+IgorDenisiuk@users.noreply.github.com>
2021-03-27 17:25:41 +0300

    Initial Commit

---
+++ TrafficLightSwiftUI/TrafficLightSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightSwiftUIApp.swift
+//  TrafficLightSwiftUI
+//
+//  Created by Igor Denisiuk on 27.03.21.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
